bbeac8d9216fa3968095970ecbfe7b2fc0b8be0f
Parthiban024 <parthiban@objectways.com>
2023-03-14 14:36:26 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+// Allow cross-origin requests from all domains
+app.use(cors());
+
+// Set up MongoDB connection
+mongoose.connect('mongodb+srv://ParthiGMR:Parthiban7548@parthibangmr.1quwer2.mongodb.net/my-database-two', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('MongoDB connected');
+});
+
+// Set up multer storage engine for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype !== 'application/pdf') {
+      cb(new Error('Resume file type must be PDF.'));
+    } else {
+      cb(null, true);
+    }
+  },
+  limits: {
+    fileSize: 700000,
+  },
+});
+
+// Add body-parser middleware to parse request body
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up API endpoint for form submission
+app.post('/submit', upload.single('resume'), async (req, res) => {
+  // Create a new document in the MongoDB collection using Mongoose
+  const { firstName, lastName, email, phone, company, degree, year, experience } = req.body;
+  const resume = req.file.path;
+
+  const data = { firstName, lastName, email, phone, company, degree, year, resume, experience }; // Add experience field
+  const newEntry = new FormEntry(data);
+  
+  try {
+    await newEntry.save();
+    res.send('Form submitted successfully');
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server error');
+  }
+  
+});
+
+// Define Mongoose schema for form data
+const formSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: Number,
+  company: String,
+  degree: String,
+  year: String,
+  resume: String,
+  experience: String, // Add this field
+});
+
+
+const FormEntry = mongoose.model('FormEntry', formSchema);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
